834f89e60583bc11d81c4c4b39d9415976b7a6c7
Sergey <soochkov.sergey@gmail.com>
2018-01-13 17:06:56 +0300

    Ok

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,31 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare let Office: any;
+declare let app: any;
+declare let $: any;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+function launch() {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+}
+
+
+if (window.hasOwnProperty('Office')
+  //&& window.hasOwnProperty('Mailbox')
+) {
+  Office.initialize = reason => {
+    $(document).ready(function () {
+      app.initialize();
+    });
+    launch();
+  }
+}
+else {
+  launch();
+}
+
