cd1e68b422db567d39ecc21eacbbc5fecccec5f9
SergeyFirst <soochkov.sergey@gmail.com>
2018-01-26 14:50:08 +0300

    Ok

--- ts_app/comments.service.ts
+++ ts_app/comments.service.ts
@@ -17,28 +17,38 @@ export class CommentsService {
     }
 
     getComments(id:string, email:string):Observable<Comment[]> {
-        return this.http.get(this.configData.commentsURL + id + "/" + email).map(res => res.json());
+        return this.http.get(this.configData.commentsURL + id + "/" + email).map(
+            res => res.json()
+        );
     }
 
     getDocuments(id:string, email:string):Observable<Document[]> {
-        return this.http.get(this.configData.documentsURL + id + "/" + email).map(res => res.json());
+        return this.http.get(this.configData.documentsURL + id + "/" + email).map(
+            res => res.json()
+        );
     }
 
     isAgreementAvailable(id:string, email:string):Observable<boolean> {
-        return this.http.get(this.configData.isAgreementAvailableURL + id + "/" + email).map(res => res.json());
+        return this.http.get(this.configData.isAgreementAvailableURL + id + "/" + email).map(
+            res => res.json()
+        );
     }
     agree(id:string, email:string, comment:string):Observable<boolean> {
         let jdoc = '{"id": "' + id + '"' +
                    ' "email": "' + email + '"' + 
                    ' "comment": "' + comment + '"}';
-        return this.http.post(this.configData.agreeURL, jdoc).map(res => res.json());
+        return this.http.post(this.configData.agreeURL, jdoc).map(
+            res => res.json()
+        );
     }
 
     disagree(id:string, email:string, comment:string):Observable<boolean> {
         let jdoc = '{"id": "' + id + '"' +
                    ' "email": "' + email + '"' + 
                    ' "comment": "' + comment + '"}';
-        return this.http.post(this.configData.disagreeURL, jdoc).map(res => res.json());
+        return this.http.post(this.configData.disagreeURL, jdoc).map(
+            res => res.json()
+        );
     }
     
 }
